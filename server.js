5cf9ed6e70613be954a0308dcca0f1121c4a5c53
Aseel <aseel.almutareb@gmail.com>
2022-06-14 11:59:37 +0200

    jwt token created

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ require('dotenv').config();
 // import express package
 const express = require('express');
 
+// import jsonwebtoken for authorization
+const jwt = require('jsonwebtoken');
+
 // instantiate express application
 const app = express();
+
+// use express body parser middleware
+app.use(express.json());
 
 // initialize posts dataset
 const posts = [
@@ -29,3 +35,19 @@ app.listen(
     PORT, 
     () => console.log(`Application is connected and listening to port ${PORT}`)
     );
+
+// create a login route
+app.post('/login', (req,res) => {
+    //Authentication: e.g. 'passport js'
+    //Authorization: JWT
+    
+    //create a token to access api's you are authorized to:
+    const username = req.body.username;
+    const user = {
+        name: username
+    };
+    const accessToken = jwt.sign(user, process.env.ACCESS_SECRET);
+    res.json({
+        accessToken
+    });
+}); 
